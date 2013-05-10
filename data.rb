a6fe4f5af0d9ebb919bb1cb3c0328246090cb3f3
Mefisto <admin@nwn.net.pl>
2013-05-10 16:30:50 +0200

    KK NWN date converter added

---
+++ data.rb
@@ -0,0 +1,63 @@
+module KK_NWN_Data
+
+  def getNWNDay(day)
+   
+       if day%10 == 0
+       
+           return "27"
+           
+       elsif day == 31
+   
+           return "30"
+           
+       else
+       
+           pom = ((day%10)-1)*3;
+       end
+
+       poczatek = (pom+1).to_s
+       koniec = (pom+3).to_s
+
+       return poczatek + " - " + koniec
+  end
+       
+  def getMonth(day,month)
+   
+       x = month%4;
+       z = ((day-1)/10)+1;
+       
+       if z == 4 
+         z = 3
+       end
+         
+       y = 3*x + z
+
+       return y
+  end
+   
+  def getNWNYear(month, year)
+
+    baza = 1480 + ((year - 2008) * 3)
+
+    if (month >= 4 &&  month < 8)
+      
+        baza  +=1
+          
+    elsif month >= 8
+      
+        baza += 2
+    end
+    
+    return baza+100
+  end
+
+ 
+  def getNWNDate(day,month,year)
+  return getNWNDay(day) + " " + getMount(day,month) + " " + getNWNYear(month,year) + "r."
+  end
+
+end 
+
+time = Time.new
+puts KK_NWN_Data.getNWNDay(time.day(), time.month(), time.year())  
+   
