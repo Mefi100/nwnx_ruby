a820e94f3f9def68c37b15ab91201220d0bb8392
Mefisto <admin@nwn.net.pl>
2013-05-13 14:15:05 +0200

    test #2

--- drop/drop_mf_szkielet.rb
+++ drop/drop_mf_szkielet.rb
@@ -1,7 +1,8 @@
 include NWScript
 
 nRandom = rand(100)+1
+puts nRandom
 
- if nRandom <= 5
- CreateItemOnObject("nw_it_msmlmisc13", $OBJECT_SELF, 1)
- end
+# if nRandom <= 5
+# CreateItemOnObject("nw_it_msmlmisc13", $OBJECT_SELF, 1)
+# end
