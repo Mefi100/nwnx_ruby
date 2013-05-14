ceab74a7f04dc6a0ec9ff145c11421e2a9ca2096
Mefisto <admin@nwn.net.pl>
2013-05-14 22:11:41 +0200

    print date for players

--- print_date.rb
+++ print_date.rb
@@ -2,7 +2,6 @@ require 'data'
 include KK_NWN_Data
 include NWScript
 
-def get_date
 time = Time.new
 dzien = getNWNDay(time.day)
 miesiac = getMonth(time.day,time.month) 
@@ -24,5 +23,4 @@ miesiace = ["Młodego Stycznia", "Sennego Lutego", "Budzącego Marca", "Wielobar
  "Srebrzystego Września", "Leniwego Października", "Sennego Listopada", "Starego Grudnia"]
 
 data = "Aktualnie jest "+dzien_miesiaca.to_s+" "+miesiace[miesiac-1]+" "+rok.to_s+" r."
-return data
-end
+SendMessageToPC(data)
