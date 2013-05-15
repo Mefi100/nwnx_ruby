31bcf5e27e36eb8369a07369121bdd77f1ad1826
Mefisto <admin@nwn.net.pl>
2013-05-15 19:43:22 +0200

    print date!

--- print_date.rb
+++ print_date.rb
@@ -23,5 +23,5 @@ miesiace = ["Młodego Stycznia", "Sennego Lutego", "Budzącego Marca", "Wielobar
  "Srebrzystego Września", "Leniwego Października", "Sennego Listopada", "Starego Grudnia"]
 
 data = "Aktualnie jest "+dzien_miesiaca.to_s+" "+miesiace[miesiac-1]+" "+rok.to_s+" r."
-SendMessageToPC($OBJECT_SELF, data)
+NWScript.SendMessageToPC($OBJECT_SELF, data)
 puts data
