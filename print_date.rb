bc98fb2a3c7e8ab46ee7d264a5db6c7151d037c1
Mefisto <admin@nwn.net.pl>
2013-05-15 07:40:44 +0200

    fixes

--- print_date.rb
+++ print_date.rb
@@ -1,6 +1,5 @@
 require 'data'
 include KK_NWN_Data
-include NWScript
 
 time = Time.new
 dzien = getNWNDay(time.day)
@@ -23,5 +22,4 @@ miesiace = ["Młodego Stycznia", "Sennego Lutego", "Budzącego Marca", "Wielobar
  "Srebrzystego Września", "Leniwego Października", "Sennego Listopada", "Starego Grudnia"]
 
 data = "Aktualnie jest "+dzien_miesiaca.to_s+" "+miesiace[miesiac-1]+" "+rok.to_s+" r."
-oPC = GetEnteringObject()
-SendMessageToPC(oPC, data)
+return data
