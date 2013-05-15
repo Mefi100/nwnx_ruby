c7207148baf950b91ee7026add68a20e7e7ef756
Mefisto <admin@nwn.net.pl>
2013-05-15 08:07:20 +0200

    test

--- print_date.rb
+++ print_date.rb
@@ -23,4 +23,5 @@ miesiace = ["Młodego Stycznia", "Sennego Lutego", "Budzącego Marca", "Wielobar
  "Srebrzystego Września", "Leniwego Października", "Sennego Listopada", "Starego Grudnia"]
 
 data = "Aktualnie jest "+dzien_miesiaca.to_s+" "+miesiace[miesiac-1]+" "+rok.to_s+" r."
-SendMessageToAllDMs(data)
+oPC = GetEnteringObject()
+SendMessageToAllDMs(oPC, data)
