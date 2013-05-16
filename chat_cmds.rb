cb8e0121fdc196bb1f2d52e89c08de150a71c858
Mefisto <admin@nwn.net.pl>
2013-05-16 16:10:14 +0200

    chat cmds

--- chat_cmds.rb
+++ chat_cmds.rb
@@ -14,59 +14,66 @@ end
 if sMessage[0, 1] == "#" 
   sMessage = sMessage.split(' ')
   
-  if sMessage[0] == "#kk"
+  sCmd = sMessage[0]
+  sMsg = sMessage[1]
+  
+  if sCmd == "#kk"
     #TO DO: Run Exec CMD for oChatter
     
-  elsif sMessage[0] == "#tow"
+  elsif sCmd == "#tow"
     SetPCChatVolume(TALKVOLUME_SILENT_TALK)
     oTowarzysz = GetHenchman(oChatter) 
     if OBJECT_INVALID != oTowarzysz
-      AssignCommand(oTowarzysz, SpeakString(sMessage[1]))
+      AssignCommand(oTowarzysz, SpeakString(sMsg))
     end   
     
-  elsif sMessage[0] == "#prz"
+  elsif sCmd == "#prz"
     SetPCChatVolume(TALKVOLUME_SILENT_TALK)
-    AssociateTalk(oChatter, ASSOCIATE_TYPE_SUMMONED, sMessage[1]) 
+    AssociateTalk(oChatter, ASSOCIATE_TYPE_SUMMONED, sMsg) 
 
-  elsif sMessage[0] == "#dom"
+  elsif sCmd == "#dom"
     SetPCChatVolume(TALKVOLUME_SILENT_TALK)
-    AssociateTalk(oChatter, ASSOCIATE_TYPE_DOMINATED, sMessage[1])
+    AssociateTalk(oChatter, ASSOCIATE_TYPE_DOMINATED, sMsg)
     
-  elsif sMessage[0] == "#chow"
+  elsif sCmd == "#chow"
     SetPCChatVolume(TALKVOLUME_SILENT_TALK)
-    AssociateTalk(oChatter, ASSOCIATE_TYPE_FAMILIAR, sMessage[1])
+    AssociateTalk(oChatter, ASSOCIATE_TYPE_FAMILIAR, sMsg)
     
-  elsif sMessage[0] == "#zw"
+  elsif sCmd == "#zw"
     SetPCChatVolume(TALKVOLUME_SILENT_TALK)
-    AssociateTalk(oChatter, ASSOCIATE_TYPE_ANIMALCOMPANION, sMessage[1])
+    AssociateTalk(oChatter, ASSOCIATE_TYPE_ANIMALCOMPANION, sMsg)
     
-  elsif sMessage[0] == "#opisz"
+  elsif sCmd == "#opisz"
     SetPCChatVolume(TALKVOLUME_SILENT_TALK)
-    SetLocalString(oChatter, "opisz", sMessage[1])
+    SetLocalString(oChatter, "opisz", sMsg)
     
-  elsif sMessage[0] == "#napisz"
+  elsif sCmd == "#napisz"
     SetPCChatVolume(TALKVOLUME_SILENT_TALK)
-    SetLocalString(oChatter, "napisz", sMessage[1])
+    SetLocalString(oChatter, "napisz", sMsg)
     
-  elsif sMessage[0] == "#nazwij" && nDM == 1
+  elsif sCmd == "#nazwij" && nDM == 1
     SetPCChatVolume(TALKVOLUME_SILENT_TALK)
     oItem =  GetItemPossessedBy(oChatter, "mf_berlo_bajerow")
-    SetLocalString(oItem, "nazwij", sMessage[1])   
+    SetLocalString(oItem, "nazwij", sMsg)   
     
-  elsif sMessage[0] == "#mob" && nDM == 1
+  elsif sCmd == "#mob" && nDM == 1
     SetPCChatVolume(TALKVOLUME_SILENT_TALK)
     lLoc = GetLocation(oChatter)
-    CreateObject(OBJECT_TYPE_CREATURE, sMessage[1], lLoc)
+    CreateObject(OBJECT_TYPE_CREATURE, sMsg, lLoc)
     
-elsif sMessage[0] == "#item" && nDM == 1
+elsif sCmd == "#item" && nDM == 1
+    SetPCChatVolume(TALKVOLUME_SILENT_TALK)
+    CreateItemOnObject(sMsg, oChatter, 1)
+    
+elsif sCmd == "#obiekt" && nDM == 1
     SetPCChatVolume(TALKVOLUME_SILENT_TALK)
     lLoc = GetLocation(oChatter)
-    CreateObject(OBJECT_TYPE_CREATURE, sMessage[1], lLoc)
+    CreateObject(OBJECT_TYPE_PLACEABLE, sMsg, lLoc)
     
-elsif sMessage[0] == "#obiekt" && nDM == 1
+elsif sCmd[0 .. 2] == "#bn" && nDM == 1
     SetPCChatVolume(TALKVOLUME_SILENT_TALK)
-    lLoc = GetLocation(oChatter)
-    CreateObject(OBJECT_TYPE_PLACEABLE, sMessage[1], lLoc)
+    oSpeaker = GetLocalObject(oChatter, "dm_speaker_"+sCmd[3 .. 3])
+    AssignCommand(oSpeaker, SpeakString(sMsg))
     
   end
   
