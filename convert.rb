189a8a159f0ff9f6d0ed63cd0866233871faeb9e
Mefisto <admin@nwn.net.pl>
2013-05-20 23:18:20 +0200

    conv

--- convert.rb
+++ convert.rb
@@ -13,32 +13,32 @@ begin
     con = Mysql.new config[1], config[3], config[5], config[7]
     
 x = 0
+i = 0
 oCWP = GetObjectByTag("RS_CREATURE",x)
 oMyArea = GetArea($OBJECT_SELF)
 
-  while(OBJECT_INVALID != GetIsObjectValid(oCWP))
+  while((OBJECT_INVALID != GetIsObjectValid(oCWP)) && i < 1000) 
             oArea = GetArea(oCWP)          
             
             if (oArea == oMyArea)
               sAreaResRef = "'"+GetResRef(oArea)+"'"
               sMobResRef =  "'"+GetName(oCWP)+"'" 
               lLoc = GetLocation(oCWP)
-              lLoc_x = lLoc.get_x().to_s
-              lLoc_y = lLoc.get_y().to_s
-              lLoc_z = lLoc.get_z().to_s
-              lLoc_o = lLoc.get_facing().to_s
+              lLoc_x = lLoc.x.to_s
+              lLoc_y = lLoc.y.to_s
+              lLoc_z = lLoc.z.to_s
+              #lLoc_o = lLoc.get_facing().to_s
               new_name = GetLocalString(oCWP, "New Name of Creature")
               action = GetLocalInt(oCWP, "Action").to_s
               time = GetLocalInt(oCWP, "Time of Day").to_s
               chance = GetLocalInt(oCWP, "Percent Chance To Spawn").to_s
               min = GetLocalInt(oCWP, "Number of Creatures Max").to_s
               max = GetLocalInt(oCWP, "Number of Creatures Min").to_s
-              puts sAreaResRef+" :: "+sMobResRef+" :: "+lLoc_x+"/"+lLoc_y+"/"+lLoc_z+" ::"
-                +new_name+" : "+action+" : "+time+" : "+chance+" "+min+" - "+max
+              puts sAreaResRef+" :: "+sMobResRef+" :: "+lLoc_x+"/"+lLoc_y+"/"+lLoc_z+" ::" +new_name+" : "+action+" : "+time+" : "+chance+" "+min+" - "+max
               #rs = con.query('INSERT INTO kk_spawn VALUES (NULL, )')  
             end
-            
-            x=x+1
+            i = i + 1
+            x = x + 1
             
             oCWP = GetObjectByTag("RS_CREATURE",x)
   end
