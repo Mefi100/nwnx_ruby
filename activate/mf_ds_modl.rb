b809489bbf256c050c075f883a5fa962dfc52139
Mefisto <admin@nwn.net.pl>
2013-05-28 10:30:46 +0200

    scroll drop fix

--- activate/mf_ds_modl.rb
+++ activate/mf_ds_modl.rb
@@ -7,28 +7,28 @@ nZwojeHi=rand(3)+4
 nRoll = rand(100)+1
 
   for i in 0..nZwoje_3 
-    sScroll = MfScrolls("A", "3")
+    sScroll = MfScrolls("D", "3")
     CreateItemOnObject(sScroll, oPC, 1)
   end
       
 
     if nZwojeHi == 4
-      sScroll = MfScrolls("A", "4")
+      sScroll = MfScrolls("D", "4")
     elsif nZwojeHi == 5
-      sScroll = MfScrolls("A", "5")
+      sScroll = MfScrolls("D", "5")
     elsif nZwojeHi == 6
-      sScroll = MfScrolls("A", "6")
+      sScroll = MfScrolls("D", "6")
     end
     
   CreateItemOnObject(sScroll, oPC, 1)
 
     if nRoll > 99
-      sScroll = MfScrolls("A", "9")
+      sScroll = MfScrolls("D", "9")
       CreateItemOnObject(sScroll, oPC, 1)
     elsif nRoll > 96
-      sScroll = MfScrolls("A", "8")
+      sScroll = MfScrolls("D", "8")
       CreateItemOnObject(sScroll, oPC, 1)
     elsif nRoll > 91
-      sScroll = MfScrolls("A", "7")
+      sScroll = MfScrolls("D", "7")
       CreateItemOnObject(sScroll, oPC, 1)
     end
