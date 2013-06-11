4ac8d2c5c3ff2d0fa762ae2fb71500e33e7878be
Mefisto <admin@nwn.net.pl>
2013-06-11 09:42:46 +0200

    cptn fix

--- drop/drop_mf_przemyt_cptn.rb
+++ drop/drop_mf_przemyt_cptn.rb
@@ -1,14 +1,14 @@
 include NWScript
 
 nRandom = rand(100)+1
-nIle = rand(3)+2
+nIle = rand(3)+1
 
- if nRandom <= 33
+ if nRandom <= 15
  CreateItemOnObject("nw_it_gem006", $OBJECT_SELF, nIle)
  end
 
 nRandom = rand(100)+1
 
- if nRandom <= 5
+ if nRandom <= 4
  CreateItemOnObject("mf_chaosik", $OBJECT_SELF, 1)
  end
